337c1b9773a59927b42108e6d86d6942679a9e96
AndreiRaducanu <143537438+AndreiRaducanu@users.noreply.github.com>
2023-09-19 22:33:52 +0300

    added dataframes

--- storia/spiders/get_json.py
+++ storia/spiders/get_json.py
@@ -2,10 +2,11 @@ import scrapy
 import json
 from datetime import datetime
 import time
+import pandas as pd
 
 class JsonSpider(scrapy.Spider):
     name = 'json_spider'
-    json_data = None
+    json_data = []
 
     def __init__(self, page, token):
         self.page = int(page)  # Convert page to an integer
@@ -40,37 +41,71 @@ class JsonSpider(scrapy.Spider):
         if 'application/json' in content_type:
             # Parse the JSON content
             json_data = response.json()
-            JsonSpider.json_data = json_data
             items_page = len(json_data['pageProps']['data']['searchAds']['items'])
             print(f'Items in page: {str(items_page)}')
-            time.sleep(3)
-            items = json_data['pageProps']['data']['searchAds']['items']
-            date_key = "dateCreated"
-            date_list = []
-            for dict in items:
-                date_list.append(dict[date_key])
+            time.sleep(0.3)
+            offer_data = []  # DataFrames
+            for offer in json_data['pageProps']['data']['searchAds']['items']:
+                try:
+                    offer_id = offer['id']
+                    title = offer['title']
+                    slug = offer['slug']
+                    area = offer['areaInSquareMeters']
+                    rooms = offer['roomsNumber']
+                    date = offer['dateCreated']
+                    location = offer['location']['reverseGeocoding']['locations'][-1].get('fullName')
+                    location = location.replace(",","|")
+                    price = offer['totalPrice'].get('value')
+
+                    # Create a DataFrame with the extracted data
+                    offer_df = pd.DataFrame({
+                        'OfferID': [offer_id],
+                        'Title': [title],
+                        'Slug': [slug],
+                        'Area': [area],
+                        'Rooms': [rooms],
+                        'Date': [date],
+                        'Location': [location],
+                        'Price': [price]
+                    })
+
+                    # Append the DataFrame to the list
+                    offer_data.append(offer_df)
+                except (KeyError, ValueError):
+                    print("Json invalid")
+
+            ###break
         
-            page_date_str = (date_list[len(date_list)-1])  # last date from page
-            page_date = datetime.strptime(page_date_str, '%Y-%m-%d %H:%M:%S')
+            page_date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
             today = datetime.today()
             result_day = today.day - page_date.day
             print(f'Page_date: {page_date.day}\n')
             print(f'Result day: {result_day}')
+
+            
             
             if result_day == 0:
                 # Increment the page number and run the Spider again
-                with open(f'output_page_{self.page}.json', 'w', encoding='utf-8') as json_file:
-                    json.dump(json_data, json_file, ensure_ascii=False, indent=4)
                 self.page += 1
                 next_page_url = response.url.split('&page=')[0] + f'&page={self.page}'
                 yield response.follow(next_page_url, self.parse)
-            else:
-                # Save JSON data to a file
-                with open(f'output_page_{self.page}.json', 'w', encoding='utf-8') as json_file:
-                    json.dump(json_data, json_file, ensure_ascii=False, indent=4)
             
-                yield {
-                    'json_data': json_data
-                }
+            # Concatenate the list of DataFrames into a single DataFrame
+            combined_df = pd.concat(offer_data, ignore_index=True)
+            
+            # Append the DataFrame to the list
+            JsonSpider.json_data.append(combined_df)
+            
         else:
             self.logger.warning(f'Invalid content type: {content_type}')
+    
+    def closed(self, reason):
+        self.logger.info("Spider closed: %s", reason)
+        combine_data()  # Call combine_data when the spider is closed
+
+# After crawling all pages, you can combine the data into a single DataFrame
+def combine_data():
+    df = pd.concat(JsonSpider.json_data, ignore_index=True)
+    df.to_csv('combined_data.csv', index=False)
+
+# You can run the spider and then call combine_data() to combine the data
