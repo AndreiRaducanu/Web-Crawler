718a022f78c511d98f1a57293a9201fb27dc85c3
AndreiRaducanu <143537438+AndreiRaducanu@users.noreply.github.com>
2023-09-19 21:21:18 +0300

    added datetime

--- storia/spiders/get_json.py
+++ storia/spiders/get_json.py
@@ -1,18 +1,17 @@
 import scrapy
 import json
+from datetime import datetime
+import time
 
 class JsonSpider(scrapy.Spider):
     name = 'json_spider'
     json_data = None
 
     def __init__(self, page, token):
-        self.page = page
+        self.page = int(page)  # Convert page to an integer
         self.token = token
         self.base_url = 'https://www.storia.ro/_next/data/' + self.token + '/ro/rezultate/inchiriere/apartament/bucuresti.json'
     
-    # Define the base URL without the page number
-
-    # Define the custom query string
     custom_query = {
         "market": "ALL",
         "ownerTypeSingleSelect": "ALL",
@@ -27,9 +26,6 @@ class JsonSpider(scrapy.Spider):
     }
 
     def start_requests(self):
-        # You can specify the page number as an argument when running the Spider
-        
-        
         # Update the custom query with the page number
         self.custom_query['page'] = str(self.page)
         
@@ -45,13 +41,36 @@ class JsonSpider(scrapy.Spider):
             # Parse the JSON content
             json_data = response.json()
             JsonSpider.json_data = json_data
+            items_page = len(json_data['pageProps']['data']['searchAds']['items'])
+            print(f'Items in page: {str(items_page)}')
+            time.sleep(3)
+            items = json_data['pageProps']['data']['searchAds']['items']
+            date_key = "dateCreated"
+            date_list = []
+            for dict in items:
+                date_list.append(dict[date_key])
+        
+            page_date_str = (date_list[len(date_list)-1])  # last date from page
+            page_date = datetime.strptime(page_date_str, '%Y-%m-%d %H:%M:%S')
+            today = datetime.today()
+            result_day = today.day - page_date.day
+            print(f'Page_date: {page_date.day}\n')
+            print(f'Result day: {result_day}')
             
-            # Save JSON data to a file
-            with open(f'data/output_page_{self.custom_query["page"]}.json', 'w', encoding='utf-8') as json_file:
-                json.dump(json_data, json_file, ensure_ascii=False, indent=4)
+            if result_day == 0:
+                # Increment the page number and run the Spider again
+                with open(f'output_page_{self.page}.json', 'w', encoding='utf-8') as json_file:
+                    json.dump(json_data, json_file, ensure_ascii=False, indent=4)
+                self.page += 1
+                next_page_url = response.url.split('&page=')[0] + f'&page={self.page}'
+                yield response.follow(next_page_url, self.parse)
+            else:
+                # Save JSON data to a file
+                with open(f'output_page_{self.page}.json', 'w', encoding='utf-8') as json_file:
+                    json.dump(json_data, json_file, ensure_ascii=False, indent=4)
             
-            yield {
-                'json_data': json_data
-            }
+                yield {
+                    'json_data': json_data
+                }
         else:
             self.logger.warning(f'Invalid content type: {content_type}')
