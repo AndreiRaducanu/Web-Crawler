591256c1e496852bda2f2d5f188556f810458832
AndreiRaducanu <143537438+AndreiRaducanu@users.noreply.github.com>
2023-09-28 22:48:12 +0300

    dynamically update data

--- web/app.py
+++ web/app.py
@@ -2,25 +2,33 @@ from flask import Flask, render_template, request
 import pandas as pd
 import time
 import os
+import threading
 
 app = Flask(__name__)
 
+# Global variable to store the DataFrame
+df = None
 
-# Simulate data acquisition delay
-def simulate_data_acquisition():
-    print("Simulating data acquisition...")
-    time.sleep(1)  # Sleep for 30 seconds
+# Function to load the CSV data into the DataFrame
+def load_data():
+    global df
+    current_directory = os.path.dirname(__file__)
+    test_txt_path = os.path.join(current_directory, '..', 'combined_data.csv')
+    try:
+        df = pd.read_csv(test_txt_path)
+    except pd.errors.EmptyDataError:
+        print("The CSV file is empty or does not contain any data.")
 
+# Thread function to periodically reload the data
+def data_reload_thread():
+    while True:
+        load_data()
+        time.sleep(15)  # Reload data every 60 seconds
 
-# Load the CSV data into a DataFrame
-current_directory = os.path.dirname(__file__)
-test_txt_path = os.path.join(current_directory, '..', 'combined_data.csv')
-simulate_data_acquisition()
-try:
-    df = pd.read_csv(test_txt_path)
-except pd.errors.EmptyDataError:
-    print("The CSV file is empty or does not contain any data.")
-
+# Start the data reload thread
+data_thread = threading.Thread(target=data_reload_thread)
+data_thread.daemon = True
+data_thread.start()
 
 @app.route('/')
 def index():
@@ -31,8 +39,8 @@ def index():
     filtered_data = df[(df['Price'] >= min_price) & (df['Price'] <= max_price)]
 
     # Pass the filtered data to the template
-    return render_template('index.html', data=filtered_data.to_dict(orient='records'))  # noqa E501
-
+    return render_template('index.html', data=filtered_data.to_dict(orient='records'))
 
 if __name__ == '__main__':
+    load_data()  # Initial data load
     app.run(debug=True)
